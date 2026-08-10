498a37581574d6445b7b44e3aabf759d9aa790f5
mustafa-turner <mustafa.turner@samudera.id>
2026-08-10 08:58:48 +0700

    Update secrets.h

--- include/secrets.h
+++ include/secrets.h
@@ -1,5 +1,5 @@
 #pragma once
 
 // Template Blynk untuk provisioning melalui Blynk.Edgent.
-#define BLYNK_TEMPLATE_ID "TMPL6_Kz9fuiR"
+#define BLYNK_TEMPLATE_ID "TMPL****"
 #define BLYNK_TEMPLATE_NAME "ESP32 Tidal Station"
